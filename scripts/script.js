3c9b9aab9b5632133a799e46a2177cd7eac331f7
s-arbelaez <sofiaarme@unisabana.edu.co>
2026-02-18 10:39:09 -0500

    Objetos restantes

--- scripts/script.js
+++ scripts/script.js
@@ -56,3 +56,52 @@ const xiaomiTablet= new Item({
     imagen: "img/xiaomipad7.png",
     descripcion: "El Xiaomi Pad 7 es un tablet de alta gama con pantalla de 12 pulgadas, procesador Snapdragon 8 Gen 1, memoria RAM de 8GB y almacenamiento interno de 256GB. Ideal para entretenimiento, productividad y multitarea."
 });
+const jblTourSmart = new Item({
+    id: 7,
+    nombre: "JBL Tour Smart",
+    categoria: "Audífonos",
+    precio: 1774900,
+    imagen: "img/jbltoursmart.png",
+    descripcion: "Los JBL Tour Smart son audífonos inalámbricos de alta calidad que ofrecen un sonido excepcional, cancelación activa de ruido y una experiencia de escucha personalizada. Con un diseño elegante y cómodo, son ideales para música, llamadas y entretenimiento en movimiento."
+});
+const sonyWH1000XM5 = new Item({
+    id: 8,
+    nombre: "Sony WH-1000XM5",
+    categoria: "Audífonos",
+    precio: 2249900,
+    imagen: "img/sony.png",
+    descripcion: "Los Sony WH-1000XM5 son audífonos inalámbricos de alta gama que ofrecen una calidad de sonido excepcional, cancelación activa de ruido líder en la industria y una experiencia de escucha personalizada. Con un diseño elegante y cómodo, son ideales para música, llamadas y entretenimiento en movimiento."
+});
+const bose700 = new Item({
+    id: 9,
+    nombre: "Bose Bass Module 700",
+    categoria: "Bocinas",
+    precio: 4489900,
+    imagen: "img/bose700.png",
+    descripcion: "El Bose Bass Module 700 es un subwoofer inalámbrico de alta calidad diseñado para complementar los sistemas de sonido Bose. Ofrece graves profundos y potentes, mejorando la experiencia de audio en películas, música y juegos. Con un diseño elegante y compacto, es fácil de integrar en cualquier configuración de entretenimiento en el hogar."
+});
+const jblClip5 = new Item({
+    id: 10,
+    nombre: "Bocina Portátil JBL Clip 5 Bluetooth, Negro",
+    categoria: "Bocinas",
+    precio: 129900,
+    imagen: "img/jblclip5.png",
+    descripcion: "La bocina portátil JBL Clip 5 es una opción ideal para disfrutar de música en cualquier lugar. Con una potencia de sonido impresionante y una duración de batería prolongada, es perfecta para actividades al aire libre. Su diseño resistente y portátil la hace ideal para aventuras al aire libre."
+});
+const samsungGalaxyWatch8 = new Item({
+    id: 11,
+    nombre: "Reloj Samsung Galaxy Watch 8 44Mm Gris Oscuro",
+    categoria: "Smartwatches",
+    precio: 1299900,
+    imagen: "img/samsungwatch8.png",
+    descripcion: "El Samsung Galaxy Watch 8 es un reloj inteligente de alta gama que combina un diseño elegante con una amplia gama de funciones. Con una pantalla AMOLED vibrante, seguimiento de salud avanzado y compatibilidad con aplicaciones, es el compañero perfecto para aquellos que buscan un dispositivo inteligente para mejorar su estilo de vida."
+});
+const appleWatchUltra3 = new Item({
+    id: 12,
+    nombre: "Apple Watch Ultra 3 Correa Alpine",
+    categoria: "Smartwatches",
+    precio: 4499900,
+    imagen: "img/applewatchultra.png",
+    descripcion: "El Apple Watch Ultra 3 con Correa Alpine es un reloj inteligente de alta gama diseñado para ofrecer un rendimiento excepcional y una experiencia de usuario premium. Con características avanzadas de salud, seguimiento de actividad física y una pantalla vibrante, es el compañero perfecto para aquellos que buscan un dispositivo inteligente para mejorar su estilo de vida."
+});
+const items = [iphone14, galaxyS23Ultra, hpVictus, lenovoLegionpro, ipad, xiaomiTablet, jblTourSmart, sonyWH1000XM5, bose700, jblClip5, samsungGalaxyWatch8, appleWatchUltra3];
